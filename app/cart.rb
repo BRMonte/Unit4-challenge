1eef2063625194c53d5c27f3a9b7a5228ebea19e
Bruno Montebello <brunormontebello@hotmail.com>
2022-07-23 14:57:23 +0100

    Set Cart business logic

--- app/cart.rb
+++ app/cart.rb
@@ -7,10 +7,27 @@ class Cart
     @product_prices = products_with_prices(products)
   end
 
+  def total(order)
+    apply_discounts(sum_without_discounts(order), order)
+  end
+
   private
 
   def products_with_prices(products)
-    products.map { |product| [product.code, product.price] }
-      .to_h
+    products.map { |product| [product.code, product.price] }.to_h
+  end
+
+  def sum_without_discounts(order)
+    order.reduce(0) {|sum, (item, num)| sum += cost_for(item, num)}
+  end
+
+  def apply_discounts(cost_before_discounts, order)
+    promotional_rules.reduce(cost_before_discounts) do |current_total, rule|
+      current_total - rule.apply(current_total, order)
+    end
+  end
+
+  def cost_for(item, num)
+    product_prices[item] * num
   end
 end
