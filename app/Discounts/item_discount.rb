0d5e60c7ac59862e22191269638e7df3db5b67ba
Bruno Montebello <brunormontebello@hotmail.com>
2022-07-23 15:12:08 +0100

    Set item discount model

---
+++ app/Discounts/item_discount.rb
@@ -0,0 +1,24 @@
+class ItemDiscount
+
+  attr_reader :item_code, :min_items, :discount
+
+  def initialize(item_code:, min_items:, discount:)
+    @item_code = item_code
+    @min_items = min_items
+    @discount = discount
+  end
+
+  def apply(current_total, order)
+    should_discount_be_applied?(order) ? apply_discount(order) : 0
+  end
+
+  private
+
+  def should_discount_be_applied?(order)
+    order[item_code] >= min_items
+  end
+
+  def apply_discount(order)
+    order[item_code] * discount
+  end
+end
