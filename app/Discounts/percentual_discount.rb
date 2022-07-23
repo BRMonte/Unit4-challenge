a365ede328fc14ff49c6e37ddc496d907f86f12d
Bruno Montebello <brunormontebello@hotmail.com>
2022-07-23 15:12:27 +0100

    Set percentual discount model

---
+++ app/Discounts/percentual_discount.rb
@@ -0,0 +1,23 @@
+class PercentDiscount
+
+  attr_reader :percent_discount, :threshold
+
+  def initialize(percent_discount:, threshold:)
+    @percent_discount = percent_discount
+    @threshold = threshold
+  end
+
+  def apply(current_total, order)
+    should_discount_be_applied?(current_total) ? apply_discount(current_total) : 0
+  end
+
+  private
+
+  def should_discount_be_applied?(current_total)
+    current_total > threshold
+  end
+
+  def apply_discount(current_total)
+    current_total * percent_discount / 100
+  end
+end
